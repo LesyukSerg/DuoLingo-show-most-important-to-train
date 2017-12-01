5d3f372f28211037e6d78af0595ef679b4a6cfe9
Sergiy <seranatles@gmail.com>
2017-12-01 23:36:33 +0200

    Update dl_feature.user.js

--- dl_feature.user.js
+++ dl_feature.user.js
@@ -4,6 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
+// @updateURL   https://github.com/LesyukSerg/DuoLingo_important-to-train/raw/master/dl_feature.meta.js
+// @downloadURL https://github.com/LesyukSerg/DuoLingo_important-to-train/raw/master/dl_feature.user.js
 
 // @match        https://www.duolingo.com/
 // @grant        none
