8b4712bc252d41b308c604588a5bf22a0fe7bf85
Sergiy <seranatles@gmail.com>
2017-12-01 23:33:58 +0200

    Create dl_feature.user.js

---
+++ dl_feature.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name			DuoLingo show important
+// @namespace		duolingo
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @updateURL   https://download.sf-helper.com/chrome/helper.meta.js
+// @downloadURL https://download.sf-helper.com/chrome/helper.user.js
+// @match        https://www.duolingo.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+   function ksort(obj) {
+       var keys = Object.keys(obj).sort(), sortedObj = {};
+
+       for (var i in keys) {
+           sortedObj[keys[i]] = obj[keys[i]];
+       }
+
+       return sortedObj;
+   }
+
+    var elements = {};
+    var polygons = document.getElementsByClassName('_25x7O');
+
+    for (var i = 0; i < polygons.length; i++) {
+        var pos = polygons[i].points[0].x;
+        var el = polygons[i].parentNode.parentNode.parentNode.parentNode;
+        elements[pos] = el;
+    }
+    elements = ksort(elements);
+
+    for (i in elements) {
+        elements[i].style = "position:absolute;top:40%;left:40%;transform:scale(3,3);background-color:#FFF;border:1px solid #e9e9e9;border-radius:8px;padding:5px;box-shadow: 0 0 10px #78C800";
+        break;
+    }
+})();
