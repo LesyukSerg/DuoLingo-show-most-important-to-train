404a6255bf4c320f2c5545befad163833d8a769b
Sergiy <seranatles@gmail.com>
2017-12-01 23:37:32 +0200

    Create dl_feature.meta.js

---
+++ dl_feature.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name			DuoLingo show important
+// @namespace		duolingo
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @updateURL   https://github.com/LesyukSerg/DuoLingo_important-to-train/raw/master/dl_feature.meta.js
+// @downloadURL https://github.com/LesyukSerg/DuoLingo_important-to-train/raw/master/dl_feature.user.js
+
+// @match        https://www.duolingo.com/
+// @grant        none
+// ==/UserScript==
